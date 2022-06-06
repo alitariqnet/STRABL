ad5242a4254f698e8edba7a1b0374621cc557f24
Ali Tariq <alibabapk@aol.com>
2022-06-06 16:01:36 +0500

    Update

--- STRABL/src/main/java/com/STRABLAPP/security/ApplicationSecurityConfig.java
+++ STRABL/src/main/java/com/STRABLAPP/security/ApplicationSecurityConfig.java
@@ -1,21 +1,23 @@
-//package com.STRABLAPP.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@SuppressWarnings("deprecation")
-//@Configuration
-//@EnableWebSecurity
-//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception{
-//		http
-//				.authorizeRequests()
-//				.anyRequest()
-//				.authenticated()
-//				.and()
-//				.httpBasic();
-//	}
-//}
+package com.STRABLAPP.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+				.authorizeRequests()
+				.antMatchers("/","index","/css/*","/js/*")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.httpBasic();
+	}
+}
