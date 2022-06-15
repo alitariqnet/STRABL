6664ee4cdba10c71fcce1c510a4a7ca3ac9f12b8
Ali Tariq <alibabapk@aol.com>
2022-06-15 23:30:13 +0500

    update

--- STRABL/src/main/java/com/STRABLAPP/StrablApplication.java
+++ STRABL/src/main/java/com/STRABLAPP/StrablApplication.java
@@ -2,12 +2,21 @@ package com.STRABLAPP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StrablApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StrablApplication.class, args);
+		ApplicationContext apc = SpringApplication.run(StrablApplication.class, args);
+		for (String s: apc.getBeanDefinitionNames())
+		System.out.println(s);
+	}
+	
+	@Bean(name="STRABL")
+	public String getName() {
+		return "STRABL";
 	}
 
 }
--- STRABL/src/main/java/com/STRABLAPP/security/ApplicationSecurityConfig.java
+++ STRABL/src/main/java/com/STRABLAPP/security/ApplicationSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.STRABLAPP.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -20,4 +22,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 				.and()
 				.httpBasic();
 	}
+	
+//	@Bean
+//	@Override
+//	protected UserDetailsService userDetailsService() {
+//
+//	}
 }
